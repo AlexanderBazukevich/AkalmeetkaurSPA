ab5c209708fd92a4612ab4e36e008fd865cd3b42
DeLaut <54510364+DeLaut@users.noreply.github.com>
2020-11-07 12:00:15 +0300

    added: library pagination

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { Book } from 'src/app/interfaces';
 import { BookService } from 'src/app/services/book/book.service';
+import { SliderService } from 'src/app/services/slider/slider.service';
 
 @Component({
   selector: 'app-book',
@@ -9,11 +10,18 @@ import { BookService } from 'src/app/services/book/book.service';
 })
 export class BookComponent implements OnInit {
 
+  @Input() currentPage: number;
   books: Book[];
+  maxPage = Math.ceil(this.service.books.length / this.service.limit);
 
-  constructor(private service: BookService) { }
+  constructor(private service: BookService,  private sliderService: SliderService) { }
 
   ngOnInit(): void {
-    this.books = this.service.getBooks();
+    this.sliderService.setLastPage(this.maxPage);
+    this.books = this.service.getBooks(this.currentPage);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.books = this.service.getBooks(this.currentPage);
   }
 }
--- src/app/pages/library-page/library-page.component.ts
+++ src/app/pages/library-page/library-page.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryPageComponent implements OnInit {
 
+  page: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  setCurrentPage(value: number) {
+    this.page = value;
+  }
 }
